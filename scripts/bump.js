1023268086fef6ddad07c8488efe505611022bac
orblazer <orblazer.minecraft@gmail.com>
2021-12-20 13:09:12 +0100

    fix(dev): right update version in metadata

--- scripts/bump.js
+++ scripts/bump.js
@@ -5,6 +5,6 @@ const { version } = require('../package.json')
 // Bump version in desktop file
 const file = resolve(__dirname, '../package/metadata.desktop')
 fs.readFile(file, 'utf-8').then((data) => {
-  const updated = data.replace(/X-KDE-PluginInfo-Version=\d+\.\d+\.\d+/, 'X-KDE-PluginInfo-Version=' + version)
+  const updated = data.replace(/X-KDE-PluginInfo-Version=(.*?)\n/, `X-KDE-PluginInfo-Version=${version}\n`)
   fs.writeFile(file, updated, 'utf-8')
 })
