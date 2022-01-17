9db3f597abec3a7ad6fcfb48b476af07fbb3298c
orblazer <orblazer.minecraft@gmail.com>
2022-01-17 23:21:19 +0100

    refactor(dev): use directly source code for test
    This also allow to enable debug via env variable

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
-PLASMOID_DIR='~/.local/share/plasma/plasmoids/'
-PACKAGE_NAME=org.kde.resourcesMonitor-fork
-
 # Enable debug for Fedora
-if ! [ -z "$(cat /etc/*release | grep ^NAME | grep Fedora)" ]
+if ! [ -z "$(cat /etc/*release | grep ^NAME | grep Fedora)" ] || [ "${DEBUG^^}" == "TRUE" ]
 then
   export QT_LOGGING_RULES="*.debug=true; qt.*.debug=false"
 fi
 
-if [ -d "$(realpath "${PLASMOID_DIR/\~/$HOME}")${PACKAGE_NAME}" ]
-then
-  echo 'Install applet...'
-  kpackagetool5 -t Plasma/Applet --install package
-else
-  echo 'Update applet...'
-  kpackagetool5 -t Plasma/Applet --upgrade package
-fi
-
 echo 'Run applet...'
-if ! [ -x "$(command -v plasmoidviewer)" ]
-then
-  plasmawindowed $PACKAGE_NAME
-else
-  plasmoidviewer -a $PACKAGE_NAME
-fi
+plasmoidviewer -a $(pwd)/package
