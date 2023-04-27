5ebfe290964ab8c60986d33ad02ef29ef54adf51
orblazer <orblazer.minecraft@gmail.com>
2023-04-27 15:06:28 +0200

    chore: change extension for build archive

--- scripts/archive.js
+++ scripts/archive.js
@@ -1,16 +1,18 @@
-const fs = require('fs')
-const { resolve } = require('path')
-const archive = require('archiver')('zip')
-const { version } = require('../package.json')
+const fs = require("fs");
+const { resolve } = require("path");
+const archive = require("archiver")("zip");
+const { version } = require("../package.json");
 
-const source = resolve(__dirname, '../package')
-const output = fs.createWriteStream(resolve(__dirname, `../resourcesMonitor-fork-${version}.zip`))
+const source = resolve(__dirname, "../package");
+const output = fs.createWriteStream(
+  resolve(__dirname, `../resourcesMonitor-fork-${version}.plasmoid`)
+);
 
 // Create archive
 archive
   .directory(source, false)
   .pipe(output)
-  .on('error', (err) => {
-    throw err
-  })
-archive.finalize()
+  .on("error", (err) => {
+    throw err;
+  });
+archive.finalize();
