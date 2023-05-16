orblazer/plasma-applet-resources-monitor

commit 5d41c02189041a26a79b71c5abf22dfbced36f4b

fix(graph): per core sensors for pre plasma 5.25
fix #47

package/contents/ui/components/graph/CpuGraph.qml
@@ -1,6 +1,7 @@
 import QtQuick 2.9
 import org.kde.plasma.plasmoid 2.0
 import "./base" as RMBaseGraph
+import "../sensors" as RMSensors
 
 RMBaseGraph.SensorGraph {
     id: root
@@ -12,11 +13,10 @@ RMBaseGraph.SensorGraph {
             _updateSensors();
         }
         function onClockAgregatorChanged() {
-            _updateSensors();
+            if (!manualFrequency.needManual) {
+                _updateSensors();
+            }
         }
-    }
-    Component.onCompleted: {
-        _updateSensors();
     }
 
     // Config options
@@ -42,6 +42,22 @@ RMBaseGraph.SensorGraph {
     }
 
     function _updateSensors() {
-        sensorsModel.sensors = ["cpu/all/" + plasmoid.configuration.cpuUnit, "cpu/all/" + plasmoid.configuration.clockAgregator + "Frequency", "cpu/all/averageTemperature"];
+        sensorsModel.sensors = ["cpu/all/" + plasmoid.configuration.cpuUnit, "cpu/all/" + plasmoid.configuration.clockAgregator + "Frequency", "cpu/cpu0/temperature"];
+    }
+
+    // Manual cpu frequency handle
+    // TODO (3.0): remove this
+    _formatValue: (index, data) => {
+        if (index === 1 && manualFrequency.needManual) {
+            return manualFrequency.getFormattedValue();
+        }
+        return _defaultFormatValue(index, data);
+    }
+
+    RMSensors.CpuFrequency {
+        id: manualFrequency
+        agregator: plasmoid.configuration.clockAgregator
+
+        onReady: _updateSensors()
     }
 }

package/contents/ui/components/sensors/CpuFrequency.qml
@@ -1,0 +1,72 @@
+import QtQuick 2.0
+import org.kde.ksysguard.sensors 1.0 as Sensors
+import org.kde.ksysguard.formatter 1.0 as Formatter
+
+// TODO (3.0): remove this
+Sensors.SensorDataModel {
+    id: root
+    property int _coreCount
+
+    property string agregator: "average" // Possible value: average, minimum, maximum
+    property bool needManual
+
+    signal ready
+
+    function getFormattedValue() {
+        return Formatter.Formatter.formatValueShowNull(getValue(), 302 /* Formatter.Unit.UnitMegaHertz */);
+    }
+
+    function getValue() {
+        let values = [];
+        for (let i = 0; i < _coreCount; i++) {
+            values[i] = data(index(0, i), Sensors.SensorDataModel.Value);
+        }
+
+        // Agregate values
+        if (agregator === "average") {
+            return values.reduce((a, b) => a + b, 0) / _coreCount;
+        } else if (agregator === "minimum") {
+            return Math.min(...values);
+        } else if (agregator === "maximum") {
+            return Math.max(...values);
+        } else {
+            return undefined;
+        }
+    }
+
+    updateRateLimit: -1
+    Component.onCompleted: {
+        sensors = ["cpu/all/coreCount", "cpu/all/" + agregator + "Frequency"];
+    }
+
+    property bool _initialized
+    property Timer _timer: Timer {
+        running: enabled
+        interval: 100 // Wait to be sure all sensors metadata has retrieved
+
+        onTriggered: {
+            /// Prevent running multiple times
+            if (_initialized) {
+                return;
+            }
+            _initialized = true;
+
+            // Do nothing if all frequency is already exist
+            if (hasIndex(0, 1)) {
+                root.enabled = false;
+                root.ready();
+                return;
+            }
+
+            // Fill sensors with all cores
+            _coreCount = data(index(0, 0));
+            const sensors = [];
+            for (let i = 0; i < _coreCount; i++) {
+                sensors[i] = "cpu/cpu" + i + "/frequency";
+            }
+            root.sensors = sensors;
+            root.needManual = true;
+            root.ready();
+        }
+    }
+}

commit 9596e9a160d164e35cef1eaf5bd1d9f7a2b564f1

chore(graph): fix temperature sensor on plasma pre 5.25
This fix issue introduced by 5d41c02189041a26a79b71c5abf22dfbced36f4b

Fix #48

package/contents/ui/components/graph/CpuGraph.qml
@@ -42,7 +42,14 @@ RMBaseGraph.SensorGraph {
     }
 
     function _updateSensors() {
-        sensorsModel.sensors = ["cpu/all/" + plasmoid.configuration.cpuUnit, "cpu/all/" + plasmoid.configuration.clockAgregator + "Frequency", "cpu/cpu0/temperature"];
+        // Manual cpu frequency handle
+        // TODO (3.0): remove this
+        let frequencySensorId = "cpu/all/" + plasmoid.configuration.clockAgregator + "Frequency";
+        if (manualFrequency.needManual) {
+            frequencySensorId = "cpu/cpu0/frequency";
+        }
+        // END Manual cpu frequency handle
+        sensorsModel.sensors = ["cpu/all/" + plasmoid.configuration.cpuUnit, frequencySensorId, "cpu/cpu0/temperature"];
     }
 
     // Manual cpu frequency handle