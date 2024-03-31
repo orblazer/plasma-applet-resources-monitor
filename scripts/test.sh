0800ea8ae7ac0d2fc000c8777d9de34d4b378b15
orblazer <orblazer.minecraft@gmail.com>
2024-03-31 07:27:11 +0200

    chore(dev): build local on pack and easy set local

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,5 +5,19 @@ then
   export QT_LOGGING_RULES="*.debug=true; qt.*.debug=false"
 fi
 
+lang=$LANG
+language=$LANGUAGE
+if [ -n "$1" ]; then
+  if [[ "$1" == *"_"* ]]; then
+    mapfile -t parts < <(echo "$1" | tr '_' '\n')
+
+    lang="${1}.UTF-8"
+    language="${1}:${parts[0]}"
+  else
+    lang="${1}_${1^^}.UTF-8"
+    language="${1}_${1^^}:${1}"
+  fi
+fi
+
 echo 'Run applet...'
-plasmoidviewer -a "$(pwd)/package"
+LANGUAGE=$language LANG=$lang plasmoidviewer -a "$(pwd)/package"
